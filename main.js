d110703cfb4779e782eea21ff6f27bdc610b9c00
ollyjw <owilcocks@gmail.com>
2023-09-18 13:21:52 +0100

    created init gameboard factory with placeShip func

--- main.js
+++ main.js
@@ -3,6 +3,20 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+// import './styles.css';
+
+
+// 10x10 sqr grid, letter & num coords
+// 5 Ship classes occupy differing amount of sqrs
+// Ships cannot overlap
+// Enemy ships hidden from player sight
+// Each turn player announces coord to hit
+// Hit or miss is marked on player board
+// When all sqrs of ship hit, it sinks
+// Game over when all ships have been sunk
+
+
 
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYmF0dGxlc2hpcHMvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0ICcuL3N0eWxlcy5jc3MnO1xyXG5cclxuXHJcbi8vIDEweDEwIHNxciBncmlkLCBsZXR0ZXIgJiBudW0gY29vcmRzXHJcbi8vIDUgU2hpcCBjbGFzc2VzIG9jY3VweSBkaWZmZXJpbmcgYW1vdW50IG9mIHNxcnNcclxuLy8gU2hpcHMgY2Fubm90IG92ZXJsYXBcclxuLy8gRW5lbXkgc2hpcHMgaGlkZGVuIGZyb20gcGxheWVyIHNpZ2h0XHJcbi8vIEVhY2ggdHVybiBwbGF5ZXIgYW5ub3VuY2VzIGNvb3JkIHRvIGhpdFxyXG4vLyBIaXQgb3IgbWlzcyBpcyBtYXJrZWQgb24gcGxheWVyIGJvYXJkXHJcbi8vIFdoZW4gYWxsIHNxcnMgb2Ygc2hpcCBoaXQsIGl0IHNpbmtzXHJcbi8vIEdhbWUgb3ZlciB3aGVuIGFsbCBzaGlwcyBoYXZlIGJlZW4gc3Vua1xyXG5cclxuXHJcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
