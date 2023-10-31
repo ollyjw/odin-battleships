d5533308bdbc94fca0fe2833361c8c720024b9df
ollyjw <owilcocks@gmail.com>
2023-10-31 15:29:20 +0000

    tidied index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import Ship from "./factories/ship";
-import Gameboard from "./factories/gameboard";
-// import * as DOM from './modules/dom';
 import * as GAME from './modules/game';
 import './styles.css';
-
-// DOM.renderOuterContainer();
-// DOM.displayBoard();
 
 GAME.startGame();
 
 
-console.log(Ship('patrolBoat').shipLength);
-console.log(Gameboard().getShipLengthArray());
+// console.log(`INDEX TEST: patrol boat ship length is ${Ship('patrolBoat').shipLength}`);
+// console.log(Gameboard().getShipLengthArray());
